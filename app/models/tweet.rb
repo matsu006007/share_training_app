f6a5ec18d742202ba8039fa12e74ace02f62403b
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-02-25 16:49:41 +0900

    Update tweet.rb

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,8 +2,11 @@ class Tweet < ApplicationRecord
 
   with_options presence: true do
     validates :content
-    validates :genre_id
+    validates :genre_id, numericality: { other_than: 1 }
     validates :user
   end
 
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
 end
