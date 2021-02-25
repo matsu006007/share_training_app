fd143489a804f7ca26618885d43af3eb1237c3b1
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-02-25 16:49:35 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
 
   def index
-    @tweets = Tweet.includes(:user)
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -10,7 +10,7 @@ class TweetsController < ApplicationController
 
   def create
     @tweet = Tweet.create(tweet_params)
-    
+    redirect_to tweets_path
   end
 
   private
