5ae6f8fefbeb6b79c3a31fbeb16a186cab5a13b6
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-03-02 14:25:58 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'messages/new'
   devise_for :users
 
   root "top#index"
   
-  resources :tweets, only: [:index, :new, :create, :show]
+  resources :tweets, only: [:index, :new, :create, :show] do
+    resources :messages, only: [:create]
+  end
   resources :users, only: [:show]
+  
 end
