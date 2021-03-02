2c8dfa4b73e84a153d7db9d1fd8c6485e75116ad
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-03-02 18:58:47 +0900

    Update 20210225061544_create_tweets.rb

--- db/migrate/20210225061544_create_tweets.rb
+++ db/migrate/20210225061544_create_tweets.rb
@@ -1,6 +1,7 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
+      t.string :title,            null: false
       t.text :content,            null: false
       t.string :genre_id,         null: false
       t.references :user,         foreign_key: true
