a371f50781aa10d58f5182cc8b820996d81dd45f
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-03-15 17:59:49 +0900

    devise　記述　修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,9 +16,6 @@ class ApplicationController < ActionController::Base
   private 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :image, :comment, :target])
-  end
-
-  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :image, :comment, :target])
   end
 
