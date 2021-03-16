1567716563b5b543c22696cf1184ffcc6b159148
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-03-16 10:50:39 +0900

    Update genre.rb

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,7 +4,11 @@ class Genre < ActiveHash::Base
     { id: 2, name: 'ランニング'},
     { id: 3, name: 'ウェイトトレーニング'},
     { id: 4, name: '自重トレーニング'},
-    { id: 5, name: 'ヨガ'}
+    { id: 5, name: 'ヨガ'},
+    { id: 6, name: 'ダンス'},
+    { id: 7, name: '栄養'},
+    { id: 8, name: '相談'},
+    { id: 9, name: '大至急'}
   ]
 
   include ActiveHash::Associations
