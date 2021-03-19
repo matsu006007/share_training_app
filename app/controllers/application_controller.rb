252f65877564971784f5dc042cdc70aa149a2075
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-03-19 10:46:34 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
@@ -17,6 +18,10 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :image, :comment, :target])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :image, :comment, :target])
+  end
+
+  def basic_auth
+    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
   end
 
 end
