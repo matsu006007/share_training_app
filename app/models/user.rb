d75e55f252677b0617b13d887538f809391b7130
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-03-20 22:21:00 +0900

    パスワード　半角英数字

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
   end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX 
   mount_uploader :image, ImageUploader
 
    has_many :tweets
