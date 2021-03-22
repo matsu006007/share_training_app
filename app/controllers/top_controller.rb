a53c4d2543afa6431a00581109441df9cfd6d6c8
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-03-22 10:48:16 +0900

    Update top_controller.rb

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,14 @@
 class TopController < ApplicationController
+  before_action :Login_conf
 
   def index
+  end
+
+  private
+  def Login_conf
+    if user_signed_in?
+      redirect_to tweets_path
+    end
   end
   
 end
